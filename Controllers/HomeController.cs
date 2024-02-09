954988da4e4a81bd007ac34049fa20b6d3e63ff5
Dheeraj <dheerajkhatri@gmail.com>
2024-02-09 18:02:25 +0530

    Data from Csv feature

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -16,6 +16,7 @@ using ZXing.Common;
 using ZXing.Windows.Compatibility;
 using System.Drawing;
 using mvc4.ViewModels;
+using Humanizer;
 
 namespace mvc4.Controllers
 {
@@ -114,7 +115,6 @@ namespace mvc4.Controllers
         {
             List<InvestorModel> invM = _context.InvestorTable.ToList<InvestorModel>();
             StringBuilder sb = new StringBuilder();
-            sb.AppendLine("Id,Name,Address,Email,Contact,Date,Active");
             foreach (var inv in invM)
             {
                 sb.Append(inv.InvestorId.ToString() + ',');
@@ -122,9 +122,13 @@ namespace mvc4.Controllers
                 sb.Append(inv.InvestorAddress + ',');
                 sb.Append(inv.InvestorEmail + ',');
                 sb.Append(inv.InvestorContact + ',');
+                sb.Append(inv.InvestorLogo + ',');
+                sb.Append(inv.InvestorContactA + ',');
                 sb.Append(inv.InvestorDate.ToString() + ',');
                 sb.Append(inv.InvestorActive.ToString() + ',');
-                sb.AppendLine();
+                sb.Append(inv.FundId.ToString() + ',');
+                sb.Append(inv.ClientId.ToString() + ',');
+                sb.Append("\n");
             }
             return File(Encoding.UTF8.GetBytes(sb.ToString()),"text/csv","Investor.csv");
         }
@@ -193,6 +197,70 @@ namespace mvc4.Controllers
             _context.InvestorTable.Add(model);
             _context.SaveChanges();
             return RedirectToAction("Index", "Home");
+        }
+
+        [HttpPost]
+        public IActionResult PostFromCsv(IFormFile file)
+        {
+            string path = string.Empty;
+            try
+            {
+                if (file != null && file.Length > 0)
+                {
+                    string fname = (DateTime.Now.ToString("yyyy-MM-dd-HH-mm-ss") + file.FileName);
+                    path = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot/Csvs", fname);
+                    using (var fileStream = new FileStream(path, FileMode.Create))
+                    {
+                        file.CopyTo(fileStream);
+                    }
+                }
+                if(path != "")
+                {
+                    var csv = System.IO.File.ReadAllText(path);
+                    if(csv != null)
+                    {
+                        int i = 0;
+                        foreach(string row in csv.Split("\n")){
+                            i++;
+                            if (i == 1) continue;
+                            var item = row.Split(',');
+
+                            //sb.Append(inv.InvestorId.ToString() + ',');
+                            //sb.Append(inv.InvestorName + ',');
+                            //sb.Append(inv.InvestorAddress + ',');
+                            //sb.Append(inv.InvestorEmail + ',');
+                            //sb.Append(inv.InvestorContact + ',');
+                            //sb.Append(inv.InvestorLogo + ',');
+                            //sb.Append(inv.InvestorContactA + ',');
+                            //sb.Append(inv.InvestorDate.ToString() + ',');
+                            //sb.Append(inv.InvestorActive.ToString() + ',');
+                            //sb.Append(inv.FundId.ToString() + ',');
+                            //sb.Append(inv.ClientId.ToString() + ',');
+
+
+                            InvestorModel investor = new InvestorModel
+                            {
+                                //InvestorId = Convert.ToInt32(item[0]),
+                                InvestorName = item[1],
+                                InvestorAddress = item[2],
+                                InvestorEmail = item[3],
+                                //InvestorPassword = item[4],
+                                InvestorContact = item[4],
+                                InvestorLogo = item[5],
+                                InvestorContactA = item[6],
+                                InvestorDate = DateOnly.Parse(item[7]),
+                                InvestorActive = Convert.ToInt32(item[8]),
+                                FundId = Convert.ToInt32(item[9]),
+                                ClientId = Convert.ToInt32(item[10])
+                            };
+                            _context.InvestorTable.Add(investor);
+                            _context.SaveChanges();
+                        }
+                    }
+                }
+            }
+            catch(Exception ex) { }
+            return RedirectToAction("Index");
         }
 
         public IActionResult MyAction()
